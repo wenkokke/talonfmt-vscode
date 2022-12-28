73fa197716c274d382024ea089f944570b1de41d
Wen Kokke <wenkokke@users.noreply.github.com>
2022-12-28 16:02:29 +0000

    Fix more errors

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,7 @@ import * as vscode from 'vscode'
 import * as child_process from 'child_process'
 import * as tmp from 'tmp'
 import * as fs from 'fs'
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-const commandExists = require('command-exists').sync
+import commandExists from 'command-exists'
 
 const exeName = 'talonfmt'
 
@@ -17,38 +15,34 @@ export function activate(_context: vscode.ExtensionContext): void {
   )
 }
 
-function findManual(): string | null {
+async function findExe(): Promise<string | null> {
+  // Try the path from the extension settings:
   let talonfmtPath = vscode.workspace.getConfiguration('talonfmt').path
-  if (talonfmtPath === '') {
-    return null
+  if (talonfmtPath !== '') {
+    talonfmtPath = talonfmtPath
+      .replace('${HOME}', os.homedir)
+      .replace('${home}', os.homedir)
+      .replace(/^~/, os.homedir)
+    const talonfmtExists = await commandExists(talonfmtPath)
+    if (!talonfmtExists) {
+      vscode.window.showErrorMessage(
+        `Path to talonfmt is set to an unknown place: ${talonfmtPath}`
+      )
+      throw new Error(`Unable to find ${talonfmtPath}`)
+    }
+    return talonfmtPath
+  } else {
+    const talonfmtExists = await commandExists(exeName)
+    return talonfmtExists ? exeName : null
   }
-  talonfmtPath = talonfmtPath
-    .replace('${HOME}', os.homedir)
-    .replace('${home}', os.homedir)
-    .replace(/^~/, os.homedir)
-
-  if (!commandExists(talonfmtPath)) {
-    vscode.window.showErrorMessage(
-      `Path to talonfmt is set to an unknown place: ${talonfmtPath}`
-    )
-    throw new Error(`Unable to find ${talonfmtPath}`)
-  }
-  return talonfmtPath
-}
-
-function findLocal(): string | null {
-  if (commandExists(exeName)) {
-    return exeName
-  }
-  return null
 }
 
 async function talonfmt(
   filePath: string,
   nominalPath: string
 ): Promise<string> {
-  return new Promise((resolve, rejects) => {
-    const talonfmtPath = findManual() ?? findLocal()
+  return new Promise(async (resolve, rejects) => {
+    const talonfmtPath = await findExe()
     if (talonfmtPath === null) {
       vscode.window.showErrorMessage(`Path to talonfmt is null`)
       rejects('Unable to call talonfmt')
@@ -60,12 +54,8 @@ async function talonfmt(
       ])
       const result: Buffer[] = []
       const err: Buffer[] = []
-      cmd.stdout.on('data', data => {
-        result.push(Buffer.from(data))
-      })
-      cmd.stderr.on('data', data => {
-        err.push(Buffer.from(data))
-      })
+      cmd.stdout.on('data', data => result.push(Buffer.from(data)))
+      cmd.stderr.on('data', data => err.push(Buffer.from(data)))
       cmd.on('exit', _exitCode => {
         const output = Buffer.concat(result).toString()
         const errorMessage = Buffer.concat(err)
@@ -96,9 +86,7 @@ class TalonFormatProvider implements vscode.DocumentFormattingEditProvider {
       tmp.file(
         {prefix: '.talonfmt', tmpdir: path.dirname(document.fileName)},
         async function _tempFileCreated(tmpErr, tmpPath, _fd, cleanupCallback) {
-          if (tmpErr) {
-            throw tmpErr
-          }
+          if (tmpErr) throw tmpErr
           fs.writeFileSync(tmpPath, document.getText())
           vscode.window.showInformationMessage(
             `Formatting ${path.basename(document.fileName)}`
